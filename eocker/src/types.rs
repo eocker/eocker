a0003104a805d36c9cf00c1d7a4c83982ab86cd4
Michael Goff <thephred@gmail.com>
2021-07-13 11:10:55 -0700

    Use serde to handle try_from on MediaType.

--- eocker/src/types.rs
+++ eocker/src/types.rs
@@ -1,4 +1,7 @@
-use serde::{Deserialize, Serialize};
+use serde::{
+    de::{value, IntoDeserializer},
+    Deserialize, Serialize,
+};
 use std::convert::TryFrom;
 
 pub trait Media {
@@ -46,45 +49,10 @@ pub enum MediaType {
 }
 
 impl TryFrom<&str> for MediaType {
-    type Error = ();
+    type Error = value::Error;
 
     fn try_from(s: &str) -> Result<Self, Self::Error> {
-        match s {
-            "application/vnd.oci.descriptor.v1+json" => Ok(MediaType::OCIContentDescriptor),
-            "application/vnd.oci.image.index.v1+json" => Ok(MediaType::OCIImageIndex),
-            "application/vnd.oci.image.manifest.v1+json" => Ok(MediaType::OCIManifestSchema1),
-            "application/vnd.oci.image.config.v1+json" => Ok(MediaType::OCIConfigJSON),
-            "application/vnd.oci.image.layer.v1.tar+gzip" => Ok(MediaType::OCILayer),
-            "application/vnd.oci.image.layer.nondistributable.v1.tar+gzip" => {
-                Ok(MediaType::OCIRestrictedLayer)
-            }
-            "application/vnd.oci.image.layer.v1.tar" => Ok(MediaType::OCIUncompressedLayer),
-            "application/vnd.oci.image.layer.nondistributable.v1.tar" => {
-                Ok(MediaType::OCIUncompressedRestrictedLayer)
-            }
-            "application/vnd.docker.distribution.manifest.v1+json" => {
-                Ok(MediaType::DockerManifestSchema1)
-            }
-            "application/vnd.docker.distribution.manifest.v1+prettyjws" => {
-                Ok(MediaType::DockerManifestSchema1Signed)
-            }
-            "application/vnd.docker.distribution.manifest.v2+json" => {
-                Ok(MediaType::DockerManifestSchema2)
-            }
-            "application/vnd.docker.distribution.manifest.list.v2+json" => {
-                Ok(MediaType::DockerManifestList)
-            }
-            "application/vnd.docker.image.rootfs.diff.tar.gzip" => Ok(MediaType::DockerLayer),
-            "application/vnd.docker.container.image.v1+json" => Ok(MediaType::DockerConfigJSON),
-            "application/vnd.docker.plugin.v1+json" => Ok(MediaType::DockerPluginConfig),
-            "application/vnd.docker.image.rootfs.foreign.diff.tar.gzip" => {
-                Ok(MediaType::DockerForeignLayer)
-            }
-            "application/vnd.docker.image.rootfs.diff.tar" => {
-                Ok(MediaType::DockerUncompressedLayer)
-            }
-            _ => Err(()),
-        }
+        Self::deserialize(s.into_deserializer())
     }
 }
 
