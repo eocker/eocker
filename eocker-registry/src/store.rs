ac600af3bf7561aa923ae1a8b74d510c28d255be
hasheddan <georgedanielmangum@gmail.com>
2021-08-08 14:31:09 -0400

    Implement basic store with thread safe hashmap
    Implements a basic store that can be used to store blobs and manifests
    in memory in a thread safe manner. Depending on guarantees we want to
    provide, this could be optimized by using RwLock and / or only locking
    at repo level.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

---
+++ eocker-registry/src/store.rs
@@ -0,0 +1,31 @@
+use bytes::Bytes;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+#[derive(Debug, Deserialize)]
+pub struct PushQuery {
+    // TODO(hasheddan): use eocker digest
+    pub digest: String,
+}
+
+// TODO(hasheddan): consider using a RwLock
+pub type BlobStore = Arc<Mutex<HashMap<String, Bytes>>>;
+
+pub fn new_blob_store() -> BlobStore {
+    Arc::new(Mutex::new(HashMap::new()))
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Manifest {
+    pub content_type: String,
+    pub content: Bytes,
+}
+
+// TODO(hasheddan): consider using a RwLock
+pub type ManifestStore = Arc<Mutex<HashMap<String, HashMap<String, Manifest>>>>;
+
+pub fn new_manifest_store() -> ManifestStore {
+    Arc::new(Mutex::new(HashMap::new()))
+}
