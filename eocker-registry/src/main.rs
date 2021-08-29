22a997c59de0d3201de7aab0f74c5445afddb8cb
hasheddan <georgedanielmangum@gmail.com>
2021-08-28 22:19:55 -0400

    Pass upload store to filters
    Constructs and passes a thread safe upload store to filters to be used
    for chunk uploads.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- eocker-registry/src/main.rs
+++ eocker-registry/src/main.rs
@@ -9,9 +9,10 @@ mod store;
 async fn main() {
     let (tx, _) = broadcast::channel::<String>(10);
     let blob_store = store::new_blob_store();
+    let upload_store = store::new_upload_store();
     let manifest_store = store::new_manifest_store();
 
-    warp::serve(filters::registry(manifest_store, blob_store, tx))
+    warp::serve(filters::registry(manifest_store, blob_store, upload_store,  tx))
         .run(([127, 0, 0, 1], 8080))
         .await;
 }
